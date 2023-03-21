0298b54496fbc64e210ce537bb5811759828b8df
fantasticlife <michael@smethur.st:wq>
2023-03-21 20:47:33 +0000

    new method to get current committees

--- app/models/committee_type.rb
+++ app/models/committee_type.rb
@@ -12,4 +12,16 @@ class CommitteeType < ApplicationRecord
       "
     )
   end
+  
+  def current_committees
+    Committee.find_by_sql(
+      "
+        SELECT c.*
+        FROM committees c, committee_committee_types cct
+        WHERE c.id = cct.committee_id
+        AND cct.committee_type_id = #{self.id}
+        AND c.end_on < '#{Date.today}'
+      "
+    )
+  end
 end
