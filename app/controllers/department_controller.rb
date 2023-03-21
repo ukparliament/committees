e6d8e7654f4f733fa5b20690140116574ae8cefa
fantasticlife <michael@smethur.st:wq>
2023-03-21 13:01:27 +0000

    new department controller

---
+++ app/controllers/department_controller.rb
@@ -0,0 +1,13 @@
+class DepartmentController < ApplicationController
+  
+  def index
+    @page_title = 'Departments'
+    @departments = Department.all.order( 'name' )
+  end
+  
+  def show
+    department = params[:department]
+    @department = Department.find_by_system_id( department )
+    @page_title = @department.name
+  end
+end
