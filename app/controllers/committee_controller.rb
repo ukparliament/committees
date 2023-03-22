f8a238b3bdf0c682140f13ce251783df6a4ae971
fantasticlife <michael@smethur.st:wq>
2023-03-22 09:25:17 +0000

    new find by sql to get sub committee count

--- app/controllers/committee_controller.rb
+++ app/controllers/committee_controller.rb
@@ -7,7 +7,22 @@ class CommitteeController < ApplicationController
   
   def current
     @page_title = 'Current committees'
-    @committees = Committee.all.where( "end_on < ?", Date.today ).order( 'name' )
+    @committees = Committee.all.where( "end_on < ?", Date.today ).where( "parent_committee_id is null").order( 'name' )
+    @committees = Committee.find_by_sql(
+      "
+        SELECT c1.*, sub_committees.sub_committee_count
+        FROM committees c1
+        LEFT JOIN (
+          SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
+          FROM committees c2
+          GROUP BY c2.parent_committee_id
+        ) sub_committees
+        ON c1.id = sub_committees.parent_committee_id
+        WHERE c1.end_on <= '#{Date.today}'
+        AND c1.parent_committee_id is null
+        ORDER BY c1.name;
+      "
+    )
   end
   
   def show
