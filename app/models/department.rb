cd8b502f391b00ba66c66c8a5b5183b452cd656a
fantasticlife <michael@smethur.st:wq>
2023-03-22 14:22:38 +0000

    expanded show query to get sub committee count. new method and query for current view

--- app/models/department.rb
+++ app/models/department.rb
@@ -3,10 +3,52 @@ class Department < ApplicationRecord
   def committees
     Committee.find_by_sql(
       "
-        SELECT c.*
-        FROM committees c, scrutinisings s
-        WHERE c.id = s.committee_id
-        AND s.department_id = #{self.id}
+        SELECT c1.*, sub_committees.sub_committee_count
+        FROM committees c1
+        
+        LEFT JOIN (
+          SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
+          FROM committees c2
+          GROUP BY c2.parent_committee_id
+        ) sub_committees
+        ON c1.id = sub_committees.parent_committee_id
+        
+        INNER JOIN (
+          SELECT s.committee_id as committee_id
+          FROM scrutinisings s
+          WHERE s.department_id = #{self.id}
+        ) scrutiny
+        ON c1.id = scrutiny.committee_id
+        
+        WHERE c1.parent_committee_id is null
+        ORDER BY c1.name;
+      "
+    )
+  end
+  
+  def current_committees
+    Committee.find_by_sql(
+      "
+        SELECT c1.*, sub_committees.sub_committee_count
+        FROM committees c1
+        
+        LEFT JOIN (
+          SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
+          FROM committees c2
+          GROUP BY c2.parent_committee_id
+        ) sub_committees
+        ON c1.id = sub_committees.parent_committee_id
+        
+        INNER JOIN (
+          SELECT s.committee_id as committee_id
+          FROM scrutinisings s
+          WHERE s.department_id = #{self.id}
+        ) scrutiny
+        ON c1.id = scrutiny.committee_id
+        
+        WHERE c1.end_on is null
+        AND c1.parent_committee_id is null
+        ORDER BY c1.name;
       "
     )
   end
