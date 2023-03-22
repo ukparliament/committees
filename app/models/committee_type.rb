2738767fd5b7361ba40546480bca48ea51b21691
fantasticlife <michael@smethur.st:wq>
2023-03-22 14:22:13 +0000

    expanded show query to get sub committee count. new method and query for current view

--- app/models/committee_type.rb
+++ app/models/committee_type.rb
@@ -5,10 +5,25 @@ class CommitteeType < ApplicationRecord
   def committees
     Committee.find_by_sql(
       "
-        SELECT c.*
-        FROM committees c, committee_committee_types cct
-        WHERE c.id = cct.committee_id
-        AND cct.committee_type_id = #{self.id}
+        SELECT c1.*, sub_committees.sub_committee_count
+        FROM committees c1
+        
+        LEFT JOIN (
+          SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
+          FROM committees c2
+          GROUP BY c2.parent_committee_id
+        ) sub_committees
+        ON c1.id = sub_committees.parent_committee_id
+        
+        INNER JOIN (
+          SELECT cct.committee_id as committee_id
+          FROM committee_committee_types cct
+          WHERE cct.committee_type_id = #{self.id}
+        ) committee_types
+        ON c1.id = committee_types.committee_id
+        
+        WHERE c1.parent_committee_id is null
+        ORDER BY c1.name;
       "
     )
   end
@@ -16,11 +31,26 @@ class CommitteeType < ApplicationRecord
   def current_committees
     Committee.find_by_sql(
       "
-        SELECT c.*
-        FROM committees c, committee_committee_types cct
-        WHERE c.id = cct.committee_id
-        AND cct.committee_type_id = #{self.id}
-        AND c.end_on < '#{Date.today}'
+        SELECT c1.*, sub_committees.sub_committee_count
+        FROM committees c1
+        
+        LEFT JOIN (
+          SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
+          FROM committees c2
+          GROUP BY c2.parent_committee_id
+        ) sub_committees
+        ON c1.id = sub_committees.parent_committee_id
+        
+        INNER JOIN (
+          SELECT cct.committee_id as committee_id
+          FROM committee_committee_types cct
+          WHERE cct.committee_type_id = #{self.id}
+        ) committee_types
+        ON c1.id = committee_types.committee_id
+        
+        WHERE c1.end_on is null
+        AND c1.parent_committee_id is null
+        ORDER BY c1.name;
       "
     )
   end
