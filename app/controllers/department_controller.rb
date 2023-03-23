9ba646e092a00cae50d82d65713bf96f4c2a7399
fantasticlife <michael@smethur.st:wq>
2023-03-23 09:08:30 +0000

    new methods to get current and all committees called from both index and current

--- app/controllers/department_controller.rb
+++ app/controllers/department_controller.rb
@@ -9,11 +9,15 @@ class DepartmentController < ApplicationController
     department = params[:department]
     @department = Department.find_by_system_id( department )
     @page_title = @department.name
+    @all_committees = @department.all_committees
+    @current_committees = @department.current_committees
   end
   
   def current
     department = params[:department]
     @department = Department.find_by_system_id( department )
     @page_title = @department.name
+    @all_committees = @department.all_committees
+    @current_committees = @department.current_committees
   end
 end
