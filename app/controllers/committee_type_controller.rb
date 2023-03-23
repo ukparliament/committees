a53847492a31c087527af22f12c0bf2d64f7e716
fantasticlife <michael@smethur.st:wq>
2023-03-23 08:19:57 +0000

    get all and current committes for numbering view switcher

--- app/controllers/committee_type_controller.rb
+++ app/controllers/committee_type_controller.rb
@@ -9,11 +9,15 @@ class CommitteeTypeController < ApplicationController
     committee_type = params[:committee_type]
     @committee_type = CommitteeType.find_by_system_id( committee_type )
     @page_title = @committee_type.name
+    @all_committees = @committee_type.all_committees
+    @current_committees = @committee_type.current_committees
   end
   
   def current
     committee_type = params[:committee_type]
     @committee_type = CommitteeType.find_by_system_id( committee_type )
     @page_title = @committee_type.name
+    @all_committees = @committee_type.all_committees
+    @current_committees = @committee_type.current_committees
   end
 end
