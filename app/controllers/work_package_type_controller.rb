654eb3f62570a495cc008c90591818251d440700
fantasticlife <michael@smethur.st:wq>
2023-03-25 12:50:56 +0000

    new method for current

--- app/controllers/work_package_type_controller.rb
+++ app/controllers/work_package_type_controller.rb
@@ -10,4 +10,10 @@ class WorkPackageTypeController < ApplicationController
     @work_package_type = WorkPackageType.find_by_system_id( work_package_type )
     @page_title = @work_package_type.name
   end
+  
+  def current
+    work_package_type = params[:work_package_type]
+    @work_package_type = WorkPackageType.find_by_system_id( work_package_type )
+    @page_title = @work_package_type.name
+  end
 end
