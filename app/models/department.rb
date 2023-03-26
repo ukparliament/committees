54e03726ae17aa49ce96b792f9163f3129c410be
fantasticlife <michael@smethur.st:wq>
2023-03-26 14:57:42 +0100

    current committees checks for null and end on in future

--- app/models/department.rb
+++ app/models/department.rb
@@ -6,6 +6,7 @@ class Department < ApplicationRecord
         SELECT c1.*, sub_committees.sub_committee_count
         FROM committees c1
         
+        -- We want a count of sub-commitees, if any, so we left join.
         LEFT JOIN (
           SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
           FROM committees c2
@@ -13,6 +14,7 @@ class Department < ApplicationRecord
         ) sub_committees
         ON c1.id = sub_committees.parent_committee_id
         
+        -- We only want committees scrutinising this department, so we inner join to scrutinisings.
         INNER JOIN (
           SELECT s.committee_id as committee_id
           FROM scrutinisings s
@@ -20,6 +22,7 @@ class Department < ApplicationRecord
         ) scrutiny
         ON c1.id = scrutiny.committee_id
         
+        -- We only want non-sub-committees committees, so we check parent_committee_id is null.
         WHERE c1.parent_committee_id is null
         ORDER BY c1.name;
       "
@@ -32,6 +35,7 @@ class Department < ApplicationRecord
         SELECT c1.*, sub_committees.sub_committee_count
         FROM committees c1
         
+        -- We want a count of sub-commitees, if any, so we left join.
         LEFT JOIN (
           SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
           FROM committees c2
@@ -39,6 +43,7 @@ class Department < ApplicationRecord
         ) sub_committees
         ON c1.id = sub_committees.parent_committee_id
         
+        -- We only want committees scrutinising this department, so we inner join to scrutinisings.
         INNER JOIN (
           SELECT s.committee_id as committee_id
           FROM scrutinisings s
@@ -46,7 +51,10 @@ class Department < ApplicationRecord
         ) scrutiny
         ON c1.id = scrutiny.committee_id
         
-        WHERE c1.end_on is null
+        -- We only want current committees so we check for a NULL end date or an end date in the future.
+        WHERE ( c1.end_on is NULL OR c1.end_on >= '#{Date.today}' )
+        
+        -- We only want non-sub-committees committees, so we check parent_committee_id is null.
         AND c1.parent_committee_id is null
         ORDER BY c1.name;
       "
