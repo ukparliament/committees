6d37cc5c78f0c020a76a9a8e914cb272bf0c34d8
fantasticlife <michael@smethur.st:wq>
2023-03-26 18:58:49 +0100

    new method to get committees

--- app/models/work_package.rb
+++ app/models/work_package.rb
@@ -7,4 +7,30 @@ class WorkPackage < ApplicationRecord
     dates += self.close_on.strftime( '%A %-d %B %Y') if self.close_on
     dates
   end
+  
+  def committees
+    Committee.find_by_sql(
+      "
+        SELECT c1.*, sub_committees.sub_committee_count
+        FROM committees c1
+        
+        -- We want a count of sub-commitees, if any, so we left join.
+        LEFT JOIN (
+          SELECT c2.parent_committee_id, count(c2.id) as sub_committee_count
+          FROM committees c2
+          GROUP BY c2.parent_committee_id
+        ) sub_committees
+        ON c1.id = sub_committees.parent_committee_id
+        
+        -- We only want committees associated with this work package, so we inner join to committee_work_packages.
+        INNER JOIN (
+          SELECT cwp.committee_id AS committee_id
+          FROM committee_work_packages cwp
+          WHERE cwp.work_package_id = #{self.id}
+        ) committee_work_packages
+        ON c1.id = committee_work_packages.committee_id
+        ORDER BY c1.name;
+      "
+    )
+  end
 end
