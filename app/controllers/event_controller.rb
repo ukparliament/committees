32abea34e88c7d0d5dbab27f18c160be6a576ebb
fantasticlife <michael@smethur.st:wq>
2023-03-27 21:18:00 +0100

    split into all and upcoming

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -2,6 +2,47 @@ class EventController < ApplicationController
   
   def index
     @page_title = 'Events'
-    @all_events = Event.all
+    @all_events = all_events
+    @upcoming_events = upcoming_events
+  end
+  
+  def upcoming
+    @page_title = 'Events'
+    @all_events = all_events
+    @upcoming_events = upcoming_events
+  end
+  
+  def all_events
+    Event.find_by_sql(
+      "
+        SELECT e.*, location.normalised_location_name
+        FROM events e
+        LEFT JOIN (
+          SELECT l.id as location_id, l.name as normalised_location_name
+          FROM locations l
+        ) location
+        ON e.location_id = location.location_id
+    
+        ORDER BY e.start_at desc
+      "
+    )
+  end
+  
+  def upcoming_events
+    Event.find_by_sql(
+      "
+        SELECT e.*, location.normalised_location_name
+        FROM events e
+        LEFT JOIN (
+          SELECT l.id as location_id, l.name as normalised_location_name
+          FROM locations l
+        ) location
+        ON e.location_id = location.location_id
+        
+        WHERE e.start_at >= '#{Time.now}'
+    
+        ORDER BY e.start_at desc
+      "
+    )
   end
 end
