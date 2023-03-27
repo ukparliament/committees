ca773cdd001497719be1f82772a3881259bd8b82
fantasticlife <michael@smethur.st:wq>
2023-03-27 20:43:14 +0100

    new time display method

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,14 @@ class Event < ApplicationRecord
   
   belongs_to :location, optional: true
   belongs_to :event_type
+  
+  def display_name
+    self.name || 'Untitled'
+  end
+  
+  def times
+    times = self.start_at.strftime( '%A %-d %B %Y @ %H:%M') + ' - '
+    times += self.end_at.strftime( '%A %-d %B %Y @ %H:%M') if self.end_at
+    times
+  end
 end
