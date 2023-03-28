ac92fc5582b90f0dcf0d5a897231e02927331948
fantasticlife <michael@smethur.st:wq>
2023-03-28 09:24:30 +0100

    new methods to get and show an event. event list queries now chronological

--- app/controllers/event_controller.rb
+++ app/controllers/event_controller.rb
@@ -12,6 +12,12 @@ class EventController < ApplicationController
     @upcoming_events = upcoming_events
   end
   
+  def show
+    event = params[:event]
+    @event = get_event( event )
+    @page_title = @event.display_name
+  end
+  
   def all_events
     Event.find_by_sql(
       "
@@ -23,7 +29,7 @@ class EventController < ApplicationController
         ) location
         ON e.location_id = location.location_id
     
-        ORDER BY e.start_at desc
+        ORDER BY e.start_at
       "
     )
   end
@@ -40,9 +46,34 @@ class EventController < ApplicationController
         ON e.location_id = location.location_id
         
         WHERE e.start_at >= '#{Time.now}'
+        AND e.cancelled_at is NULL
     
-        ORDER BY e.start_at desc
+        ORDER BY e.start_at
       "
     )
+  end
+  
+  def get_event( event_id )
+    Event.find_by_sql(
+        "
+          SELECT e.*, event_type.event_type_name as event_type_name, location.normalised_location_name
+          FROM events e
+          
+          INNER JOIN (
+            SELECT et.id as event_type_id, et.name as event_type_name
+            FROM event_types et
+          ) event_type
+          ON e.event_type_id = event_type.event_type_id
+          
+          LEFT JOIN (
+            SELECT l.id as location_id, l.name as normalised_location_name
+            FROM locations l
+          ) location
+          ON e.location_id = location.location_id
+      
+          WHERE e.system_id = #{event_id}
+          LIMIT 1
+        "
+      ).first
   end
 end
