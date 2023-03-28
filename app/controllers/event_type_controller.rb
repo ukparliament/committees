b053349a592c18e3f4cd2afb6b5a459f42b6b772
fantasticlife <michael@smethur.st:wq>
2023-03-28 16:47:52 +0100

    new controller and views for event types:

---
+++ app/controllers/event_type_controller.rb
@@ -0,0 +1,23 @@
+class EventTypeController < ApplicationController
+  
+  def index
+    @page_title = 'Event types'
+    @event_types = EventType.all.order( 'name' )
+  end
+  
+  def show
+    event_type = params[:event_type]
+    @event_type = EventType.find_by_system_id( event_type )
+    @page_title = @event_type.name
+    @all_events = @event_type.all_events
+    @upcoming_events = @event_type.upcoming_events
+  end
+  
+  def upcoming
+    event_type = params[:event_type]
+    @event_type = EventType.find_by_system_id( event_type )
+    @page_title = @event_type.name
+    @all_events = @event_type.all_events
+    @upcoming_events = @event_type.upcoming_events
+  end
+end
