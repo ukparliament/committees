e680d2ce63fecbab8a83d02edd5bcecf25a78d79
fantasticlife <michael@smethur.st:wq>
2023-03-28 09:20:57 +0100

    added methods to get all events and upcoming events

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -82,4 +82,57 @@ class Committee < ApplicationRecord
       "
     )
   end
+  
+  def all_events
+    Event.find_by_sql(
+      "
+        SELECT e.*, location.normalised_location_name
+        FROM events e
+        
+        LEFT JOIN (
+          SELECT l.id as location_id, l.name as normalised_location_name
+          FROM locations l
+        ) location
+        ON e.location_id = location.location_id
+        
+        -- We only want events associated with this committee, so we inner join to committee_events.
+        INNER JOIN (
+          SELECT ce.event_id as event_id
+          FROM committee_events ce
+          WHERE ce.committee_id = #{self.id}
+        ) committee_event
+        ON e.id = committee_event.event_id
+    
+        ORDER BY e.start_at
+      "
+    )
+  end
+  
+  def upcoming_events
+    Event.find_by_sql(
+      "
+        SELECT e.*, location.normalised_location_name
+        FROM events e
+        
+        LEFT JOIN (
+          SELECT l.id as location_id, l.name as normalised_location_name
+          FROM locations l
+        ) location
+        ON e.location_id = location.location_id
+        
+        -- We only want events associated with this committee, so we inner join to committee_events.
+        INNER JOIN (
+          SELECT ce.event_id as event_id
+          FROM committee_events ce
+          WHERE ce.committee_id = #{self.id}
+        ) committee_event
+        ON e.id = committee_event.event_id
+        
+        WHERE e.start_at >= '#{Time.now}'
+        AND e.cancelled_at is NULL
+    
+        ORDER BY e.start_at
+      "
+    )
+  end
 end
