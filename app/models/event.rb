50516910626743fd2ce723dbc8cb809f637157ef
fantasticlife <michael@smethur.st:wq>
2023-04-04 08:25:54 +0100

    new method to list event segments

--- app/models/event.rb
+++ app/models/event.rb
@@ -58,4 +58,26 @@ class Event < ApplicationRecord
       "
     )
   end
+  
+  def event_segments
+    EventSegment.find_by_sql(
+      "
+        SELECT es.*, e.location_name as location_name, l.name as normalised_location_name, at.name AS activity_type_name
+        FROM event_segments es
+      
+        INNER JOIN activity_types at
+        ON es.activity_type_id = at.id
+      
+        INNER JOIN events e
+        ON e.id = es.event_id
+      
+      
+        LEFT JOIN locations l
+        ON e.location_id = l.id
+      
+        WHERE es.event_id = #{self.id}
+        ORDER BY es.start_at;
+      "
+    )
+  end
 end
