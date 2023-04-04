b0b1095234af257d29c11c484f42ca472b0869e5
fantasticlife <michael@smethur.st:wq>
2023-04-04 10:56:57 +0100

    new event segment controller and views

---
+++ app/controllers/event_segment_controller.rb
@@ -0,0 +1,79 @@
+class EventSegmentController < ApplicationController
+  
+  def index
+    @all_event_segments = all_event_segments
+    @upcoming_event_segments = upcoming_event_segments
+    @page_title = 'Event segments'
+  end
+  
+  def upcoming
+    @all_event_segments = all_event_segments
+    @upcoming_event_segments = upcoming_event_segments
+    @page_title = 'Event segments'
+  end
+  
+  def show
+    event_segment = params[:event_segment]
+    @event_segment = get_event( event_segment )
+    @page_title = @event_segment.display_name
+  end
+  
+  def all_event_segments
+    EventSegment.find_by_sql(
+      "
+        SELECT es.*, e.location_name as location_name, l.name as normalised_location_name, at.name AS activity_type_name
+        FROM event_segments es
+      
+        INNER JOIN activity_types at
+        ON es.activity_type_id = at.id
+      
+        INNER JOIN events e
+        ON e.id = es.event_id
+      
+      
+        LEFT JOIN locations l
+        ON e.location_id = l.id
+      
+      
+        ORDER BY es.start_at;
+      "
+    )
+  end
+  
+  def upcoming_event_segments
+    EventSegment.find_by_sql(
+      "
+        SELECT es.*, e.location_name as location_name, l.name as normalised_location_name, at.name AS activity_type_name
+        FROM event_segments es
+        
+        INNER JOIN activity_types at
+        ON es.activity_type_id = at.id
+        
+        INNER JOIN events e
+        ON e.id = es.event_id
+        
+        
+        LEFT JOIN locations l
+        ON e.location_id = l.id
+        
+        
+        WHERE es.start_at >= '#{Time.now}'
+        ORDER BY es.start_at;
+      "
+    )
+  end
+  
+  def get_event( event_segment )
+    EventSegment.find_by_sql(
+      "
+        SELECT es.*, e.location_name as location_name, l.name as normalised_location_name
+        FROM event_segments es, events e
+        LEFT JOIN locations l
+        ON e.location_id = l.id
+        WHERE es.event_id = e.id
+        AND es.system_id = #{event_segment}
+        LIMIT 1;
+      "
+    ).first
+  end
+end
