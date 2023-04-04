600349d6ba10dd139d2688565587bcd9863e1cfc
fantasticlife <michael@smethur.st:wq>
2023-04-04 08:25:04 +0100

    new methods for all event segments and upcoming event segments

--- app/models/activity_type.rb
+++ app/models/activity_type.rb
@@ -1,5 +1,47 @@
 class ActivityType < ApplicationRecord
   
-  belongs_to :event
-  belongs_to :activity_type
+  def all_event_segments
+    EventSegment.find_by_sql(
+      "
+        SELECT es.*, e.location_name as location_name, l.name as normalised_location_name, at.name AS activity_type_name
+        FROM event_segments es
+      
+        INNER JOIN activity_types at
+        ON es.activity_type_id = at.id
+      
+        INNER JOIN events e
+        ON e.id = es.event_id
+      
+      
+        LEFT JOIN locations l
+        ON e.location_id = l.id
+      
+        WHERE es.activity_type_id = #{self.id}
+        ORDER BY es.start_at;
+      "
+    )
+  end
+  
+  def upcoming_event_segments
+    EventSegment.find_by_sql(
+      "
+        SELECT es.*, e.location_name as location_name, l.name as normalised_location_name, at.name AS activity_type_name
+        FROM event_segments es
+        
+        INNER JOIN activity_types at
+        ON es.activity_type_id = at.id
+        
+        INNER JOIN events e
+        ON e.id = es.event_id
+        
+        
+        LEFT JOIN locations l
+        ON e.location_id = l.id
+        
+        WHERE es.activity_type_id = #{self.id}
+        AND es.start_at >= '#{Time.now}'
+        ORDER BY es.start_at;
+      "
+    )
+  end
 end
