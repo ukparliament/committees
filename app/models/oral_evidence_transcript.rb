926e848d64db1d7dda747636a9a80f489ef2a065
fantasticlife <michael@smethur.st:wq>
2023-04-07 20:15:41 +0100

    added links method

--- app/models/oral_evidence_transcript.rb
+++ app/models/oral_evidence_transcript.rb
@@ -2,4 +2,8 @@ class OralEvidenceTranscript < ApplicationRecord
   
   belongs_to :event_segment, optional: true
   has_many :oral_evidence_transcript_files
+  
+  def link
+    "https://committees.parliament.uk/oralevidence/#{self.system_id}/html"
+  end
 end
