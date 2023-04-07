a867d305773dee8c3c03a41a0e7a43af399b1825
fantasticlife <michael@smethur.st:wq>
2023-04-07 20:16:30 +0100

    added methods for format display, format extension and link

--- app/models/oral_evidence_transcript_file.rb
+++ app/models/oral_evidence_transcript_file.rb
@@ -1,4 +1,32 @@
 class OralEvidenceTranscriptFile < ApplicationRecord
   
   belongs_to :oral_evidence_transcript
+  
+  def format_display
+    case self.format
+    when 'Html'
+      format_display = 'HTML'
+    when 'Pdf'
+      format_display = 'PDF'
+    when 'OriginalFormat'
+      format_display = 'Word'
+    end
+    format_display
+  end
+  
+  def format_extension
+    case self.format
+    when 'Html'
+      format_display = 'html'
+    when 'Pdf'
+      format_display = 'pdf'
+    when 'OriginalFormat'
+      format_display = 'docx'
+    end
+    format_display
+  end
+  
+  def link
+    "https://committees.parliament.uk/oralevidence/#{self.oral_evidence_transcript.system_id}/#{self.format_extension}"
+  end
 end
