d78df5c2a3434593efc780d60c4c638027e95a00
fantasticlife <michael@smethur.st:wq>
2023-04-08 17:20:14 +0100

    added work packages method

--- app/models/oral_evidence_transcript.rb
+++ app/models/oral_evidence_transcript.rb
@@ -10,6 +10,7 @@ class OralEvidenceTranscript < ApplicationRecord
         SELECT c.*, sub_committees.sub_committee_count
         FROM committees c
         
+        -- We only want committees associated with this oral evidence, so we inner join.
         INNER JOIN (
           SELECT coet.oral_evidence_transcript_id as oral_evidence_transcript_id, coet.committee_id as committee_id
           FROM committee_oral_evidence_transcripts coet
@@ -25,6 +26,25 @@ class OralEvidenceTranscript < ApplicationRecord
         ) sub_committees
         ON sub_committees.parent_committee_id = c.id
         ORDER BY c.name;
+      "
+    )
+  end
+  
+  def work_packages
+    WorkPackage.find_by_sql(
+      "
+        SELECT wp.*
+        FROM work_packages wp
+        
+        -- We only want work packages associated with this oral evidence, so we inner join.
+        INNER JOIN (
+          SELECT wpoet.oral_evidence_transcript_id as oral_evidence_transcript_id, wpoet.work_package_id as work_package_id
+          FROM work_package_oral_evidence_transcripts wpoet
+          WHERE wpoet.oral_evidence_transcript_id = #{self.id}
+        ) work_package_oral_evidence_transcript
+        ON work_package_oral_evidence_transcript.work_package_id = wp.id
+        
+        ORDER BY wp.open_on desc, wp.close_on desc
       "
     )
   end
