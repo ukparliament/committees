51c14fb1898db609380ab075c8674d511ec4e5e2
fantasticlife <michael@smethur.st:wq>
2023-04-18 13:55:03 +0100

    new organisation controller and views

---
+++ app/controllers/position_controller.rb
@@ -0,0 +1,43 @@
+class PositionController < ApplicationController
+  
+  def index
+    @page_title = 'Positions'
+    @positions = Position.find_by_sql(
+      "
+        SELECT p.*, organisation.organisation_name AS organisation_name, witness.oral_evidence_transcript_count AS oral_evidence_transcript_count
+        FROM positions p
+        
+        INNER JOIN (
+          SELECT o.id as organisation_id, o.name as organisation_name
+          FROM organisations o
+        ) organisation
+        ON organisation.organisation_id = p.organisation_id
+        
+        INNER JOIN (
+          SELECT w.position_id AS position_id, COUNT(oet.id) AS oral_evidence_transcript_count
+          FROM witnesses w, oral_evidence_transcripts oet
+          WHERE w.oral_evidence_transcript_id = oet.id
+          GROUP BY w.position_id
+        ) witness
+        ON witness.position_id = p.id
+        
+        
+        
+        ORDER BY organisation.organisation_name, p.name
+      "
+    )
+  end
+  
+  def show
+    position = params[:position]
+    @position = Position.find_by_sql(
+      "
+        SELECT p.*, o.name AS organisation_name
+        FROM positions p, organisations o
+        WHERE p.organisation_id = o.id
+        AND p.id = #{position}
+      "
+    ).first
+    @page_title = "#{@position.name} - #{@position.organisation_name}"
+  end
+end
