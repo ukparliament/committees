36ebac2a58a15f215b0d5c66bc2dcdaf4a69eeb7
fantasticlife <michael@smethur.st:wq>
2023-04-21 14:58:02 +0100

    new positions method

--- app/models/witness.rb
+++ app/models/witness.rb
@@ -2,5 +2,16 @@ class Witness < ApplicationRecord
   
   belongs_to :oral_evidence_transcript
   belongs_to :person, optional: true
-  belongs_to :position
+  
+  def positions
+    Position.find_by_sql(
+      "
+        SELECT p.*, o.name AS organisation_name
+        FROM positions p, organisations o, witness_positions wp
+        WHERE p.organisation_id = o.id
+        AND p.id = wp.position_id
+        AND wp.witness_id = #{self.id}
+      "
+    )
+  end
 end
