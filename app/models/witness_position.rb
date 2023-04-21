77ee232fc6cf03485bff016c623d42232d0a54e0
fantasticlife <michael@smethur.st:wq>
2023-04-21 14:52:46 +0100

    new witness position model

---
+++ app/models/witness_position.rb
@@ -0,0 +1,5 @@
+class WitnessPosition < ApplicationRecord
+  
+  belongs_to :witness
+  belongs_to :position
+end
---
+++ test/models/witness_position_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class WitnessPositionTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
