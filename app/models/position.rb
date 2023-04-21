ac1f9b87da01956b7856cbfab44ed5bfb655dcf4
fantasticlife <michael@smethur.st:wq>
2023-04-21 14:56:40 +0100

    added witness position join table to witnesses method

--- app/models/position.rb
+++ app/models/position.rb
@@ -6,9 +6,10 @@ class Position < ApplicationRecord
     Witness.find_by_sql(
       "
         SELECT w.*, oet.published_on as oral_evidence_transcript_published_on
-        FROM oral_evidence_transcripts oet, witnesses w
+        FROM oral_evidence_transcripts oet, witnesses w, witness_positions wp
         WHERE oet.id = w.oral_evidence_transcript_id
-        AND w.position_id = #{self.id}
+        AND w.id = wp.witness_id
+        AND wp.position_id =  #{self.id}
       "
     )
   end
