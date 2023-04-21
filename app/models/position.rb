deccb4ea8110372752d9b7d26551f2966fc5e4b6
fantasticlife <michael@smethur.st:wq>
2023-04-21 15:33:56 +0100

    expanded witnesses query to get oral evidence transcript system id

--- app/models/position.rb
+++ app/models/position.rb
@@ -5,7 +5,7 @@ class Position < ApplicationRecord
   def witnesses
     Witness.find_by_sql(
       "
-        SELECT w.*, oet.published_on as oral_evidence_transcript_published_on
+        SELECT w.*, oet.published_on AS oral_evidence_transcript_published_on, oet.system_id AS oral_evidence_transcript_system_id
         FROM oral_evidence_transcripts oet, witnesses w, witness_positions wp
         WHERE oet.id = w.oral_evidence_transcript_id
         AND w.id = wp.witness_id
