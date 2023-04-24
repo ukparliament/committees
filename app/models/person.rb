d0c2243e09c72cc7a70458c80097f3861dbe302a
fantasticlife <michael@smethur.st:wq>
2023-04-24 21:00:25 +0100

    new witnesses midel

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,2 +1,13 @@
 class Person < ApplicationRecord
+  
+  def witnesses
+    Witness.find_by_sql(
+      "
+        SELECT w.*, oet.published_on AS oral_evidence_transcript_published_on, oet.system_id AS oral_evidence_transcript_system_id
+        FROM oral_evidence_transcripts oet, witnesses w
+        WHERE oet.id = w.oral_evidence_transcript_id
+        AND w.person_id =  #{self.id}
+      "
+    )
+  end
 end
