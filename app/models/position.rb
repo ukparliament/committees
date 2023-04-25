fffc0cafed5ed2c778356a50c75ffe4ff72a0b92
fantasticlife <michael@smethur.st:wq>
2023-04-25 08:30:06 +0100

    added oral evidence transcripts method

--- app/models/position.rb
+++ app/models/position.rb
@@ -13,4 +13,17 @@ class Position < ApplicationRecord
       "
     )
   end
+  
+  def oral_evidence_transcripts
+    OralEvidenceTranscript.find_by_sql(
+      "
+        SELECT oet.*
+        FROM oral_evidence_transcripts oet, witnesses w, witness_positions wp
+        WHERE oet.id = w.oral_evidence_transcript_id
+        AND w.id = wp.witness_id
+        AND wp.position_id = #{self.id}
+        ORDER BY oet.published_on desc
+      "
+    )
+  end
 end
