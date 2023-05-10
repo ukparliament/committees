65246f2d34a50501e4fd25cdf2aaba0b7ef57b9a
fantasticlife <michael@smethur.st:wq>
2023-05-10 10:22:19 +0100

    added lifespan method

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -3,4 +3,10 @@ class Membership < ApplicationRecord
   belongs_to :committee
   belongs_to :person
   belongs_to :role
+  
+  def lifespan
+    lifespan = self.start_on.strftime( '%e %B %Y' ) + ' - '
+    lifespan += self.end_on.strftime( '%e %B %Y' ) if self.end_on
+    lifespan
+  end
 end
