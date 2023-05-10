2c8de9a3e08bdf118a6585ff942afe29b132d219
fantasticlife <michael@smethur.st:wq>
2023-05-10 10:21:52 +0100

    added method to get committee memeberships

--- app/models/person.rb
+++ app/models/person.rb
@@ -22,4 +22,17 @@ class Person < ApplicationRecord
       "
     )
   end
+  
+  def committee_memberships
+    Membership.find_by_sql(
+      "
+        SELECT m.*, c.name AS committee_name, c.system_id AS committee_system_id, r.name AS role_name
+        FROM memberships m, committees c, roles r
+        WHERE m.person_id = #{self.id}
+        AND m.committee_id = c.id
+        AND m.role_id = r.id
+        ORDER BY m.start_on desc, m.end_on desc
+      "
+    )
+  end
 end
