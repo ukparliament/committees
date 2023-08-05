85ec25289579af70b6c57a0a33b74695e82ec6b5
fantasticlife <michael@smethur.st:wq>
2023-08-05 14:39:33 +0100

    added methods for work packages and publications

--- app/models/publication.rb
+++ app/models/publication.rb
@@ -3,4 +3,36 @@ class Publication < ApplicationRecord
   belongs_to :publication_type
   belongs_to :committee
   belongs_to :department, optional: true
+  
+
+  
+  def paper_series_numbers
+    PaperSeriesNumber.find_by_sql(
+      "
+        SELECT psn.*, s.label AS session_label
+        FROM paper_series_numbers psn, sessions s
+        WHERE psn.session_id = s.id
+        AND psn.publication_id = #{self.id}
+      "
+    )
+  end
+  
+  def work_packages
+    WorkPackage.find_by_sql(
+      "
+        SELECT wp.*
+        FROM work_packages wp
+        
+        -- We only want work packages associated with this publication, so we inner join.
+        INNER JOIN (
+          SELECT wpp.publication_id as publication_id, wpp.work_package_id as work_package_id
+          FROM work_package_publications wpp
+          WHERE wpp.publication_id = #{self.id}
+        ) work_package_publication
+        ON work_package_publication.work_package_id = wp.id
+        
+        ORDER BY wp.open_on desc, wp.close_on desc
+      "
+    )
+  end
 end
