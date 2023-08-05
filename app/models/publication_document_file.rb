06f2b956925c996b138b3558496be41f30f23f4b
fantasticlife <michael@smethur.st:wq>
2023-08-05 19:41:53 +0100

    added link construction method

--- app/models/publication_document_file.rb
+++ app/models/publication_document_file.rb
@@ -1,4 +1,10 @@
 class PublicationDocumentFile < ApplicationRecord
   
   belongs_to :publication_document
+  
+  def link( publication )
+    "https://committees.parliament.uk/publications/#{publication.system_id}/documents/#{self.publication_document_system_id}/default/"
+  end
 end
+
+
