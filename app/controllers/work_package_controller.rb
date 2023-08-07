27d6f977d898256a7f2b94f66fb09190a1f5d194
fantasticlife <michael@smethur.st:wq>
2023-08-07 15:20:02 +0100

    added methods to list publication types and publications by type

--- app/controllers/work_package_controller.rb
+++ app/controllers/work_package_controller.rb
@@ -35,4 +35,19 @@ class WorkPackageController < ApplicationController
     @work_package = WorkPackage.find_by_system_id( work_package )
     @page_title = @work_package.title
   end
+  
+  def publication_type_list
+    work_package = params[:work_package]
+    @work_package = WorkPackage.find_by_system_id( work_package )
+    @page_title = @work_package.title
+  end
+  
+  def publication_type_show
+    work_package = params[:work_package]
+    @work_package = WorkPackage.find_by_system_id( work_package )
+    publication_type = params[:publication_type]
+    @publication_type = PublicationType.find_by_system_id( publication_type )
+    @page_title = @work_package.title
+    @publications = @work_package.publications_of_type( @publication_type )
+  end
 end
